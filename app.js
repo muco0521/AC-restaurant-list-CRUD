ead8f4a82339a45f440682bf52fc716cced677e5
muco0521 <muco0521@gmail.com>
2023-04-20 12:10:53 +0800

    feat: add create restaurant function

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const express = require('express')
 const app = express()
 const port = 3000
 const exphbs = require('express-handlebars')
-const restaurantList = require('./restaurant.json')
 const mongoose = require('mongoose')
 const Restaurant = require('./models/restaurant')
 
@@ -24,10 +23,10 @@ db.once('open', () => {
 })
 
 
-app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
-app.set('view engine', 'handlebars')
+app.engine('hbs', exphbs({ defaultLayout: 'main' , extname: 'hbs'}))
+app.set('view engine', 'hbs')
 app.use(express.static('public'))
-
+app.use(express.urlencoded({ extended: true }))
 
 
 app.get('/', (req, res) => {
@@ -35,6 +34,10 @@ app.get('/', (req, res) => {
     .lean()
     .then((restaurants) => res.render('index', { restaurants }))
     .catch((error) => console.error(error))
+})
+
+app.get('/restaurants/new', (req, res) => {
+  return res.render('new')
 })
 
 app.get('/restaurants/:restaurant_id', (req, res) => {
@@ -50,6 +53,13 @@ app.get('/search', (req, res) => {
       item.category.toLowerCase().includes(keyword) 
   )
   res.render('index', { restaurants: filteredRestaurants, keyword: keyword })
+})
+
+app.post('/restaurants', (req, res) => {
+  const newRestaurant = req.body
+  return Restaurant.create(newRestaurant)
+    .then(() => res.redirect('/'))
+    .catch((error) => console.log('error'))
 })
 
 app.listen(port, () => {
